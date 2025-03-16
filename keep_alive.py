6f9e76197fd3e7458fd4755b2a1310a7463a50bd
Krishna091909 <kona.krishna1997@gmail.com>
2025-03-16 21:34:28 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,1 @@
-import requests
-import asyncio
 
-# Your Render/Flask URL
-FLASK_URL = "https://tele-bot-t5yd.onrender.com"
-
-async def keep_alive():
-    while True:
-        try:
-            requests.get(FLASK_URL)
-            print("Keep-alive ping sent!")
-        except Exception as e:
-            print(f"Failed to ping: {e}")
-        await asyncio.sleep(600)  # Ping every 10 minutes
-
-if __name__ == "__main__":
-    asyncio.run(keep_alive())
